87ba5afee1b58f6ef0c88292ca0027ff8872fb14
Daniel Baird <JamesDanielBaird@gmail.com>
2014-11-24 19:54:22 -0700

    Operator Postfix

--- src/jdbaird/ShuntingYard.java
+++ src/jdbaird/ShuntingYard.java
@@ -6,13 +6,14 @@ import java.util.Stack;
 /**
  * Created by Daniel on 11/24/2014.
  * This class implements the Shunting-yard Algorithm on a list of tokens supplied by TokenMaker.java
+ * http://en.wikipedia.org/wiki/Shunting-yard_algorithm
  */
 
 public class ShuntingYard {
 
     private ArrayList<Token> tokenArrayList;
 
-    private Stack operators = new Stack();
+    private Stack<Token> operators = new Stack();
     private ArrayList<Token> outputList;
 
     public ShuntingYard(ArrayList<Token> tokenArrayList){
@@ -25,6 +26,22 @@ public class ShuntingYard {
                 case 0: //Is number
                     break;
                 case 1: //Is operator
+                        boolean operatorNotPlaced = true;
+                        while(operatorNotPlaced) { //While the operator hasn't been added to the stack
+                            if(!operators.isEmpty()){
+                                //Check PEMDAS against top of stack
+                                if (firstOperatorHasPrecedence(tokenArrayList.get(i), operators.peek())) {
+                                    //If current is higher priority than the one on the stack just put the current on the stack
+                                    operators.add(tokenArrayList.get(i));
+                                } else {
+                                    //If not, pop the top of the stack, append it to the outputList, then again try to attach the current on the stack
+                                    outputList.add(operators.pop());
+
+                                }
+                            }else{
+                                operators.add(tokenArrayList.get(i));
+                            }
+                        }
                     break;
                 case 2: //Is (
                     break;
@@ -35,6 +52,18 @@ public class ShuntingYard {
             }
 
 
+        }
+    }
+
+    private static boolean firstOperatorHasPrecedence(Token first, Token second){ //TODO Fix this, it's currently a quick hack (ONLY SUPPORTS "+-/*")
+        if(first.getValue() == "/" || first.getValue() == "*"){
+            if(second.getValue() == "/" || second.getValue() == "*"){
+                return false;
+            }else{
+                return true;
+            }
+        }else{
+            return false;
         }
     }
 
